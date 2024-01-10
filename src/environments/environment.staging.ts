81621930f673cd7da8213ca6d810eeeafc3b5cc9
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-01-10 22:45:31 +0600

    Added serve configuration

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,27 @@
+import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
+
+export const environment = {
+  production: true,
+  staging: false,
+  type: 'dev',
+  baseUrl: 'http://techinterview.space',
+  resourceApiURI: 'https://api.techinterview.space',
+  identityApiURI: 'https://techinterview-space.eu.auth0.com',
+  name: '',
+  auth: {
+    domain: 'techinterview-space.eu.auth0.com',
+    authority: 'https://techinterview-space.eu.auth0.com',
+    client_id: 'juNFNVr0wy1yayFgM2KTbk8374oP8MDk',
+    redirect_uri: 'http://localhost:4200/auth-callback',
+    post_logout_redirect_uri: 'http://localhost:4200/logout-callback',
+    response_type: 'id_token token',
+    scope: 'openid profile name given_name family_name email nickname',
+    filterProtocolClaims: true,
+    loadUserInfo: true,
+    automaticSilentRenew: true,
+    silent_redirect_uri: 'http://localhost:4200/silent-refresh.html'
+  },
+  googleAnalytics: {
+    imports: []
+  }
+};
