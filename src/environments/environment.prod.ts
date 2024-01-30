717d493302c7947b7bf9d9dca985e2178cda7056
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-01-30 19:33:01 +0600

    set to true

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-go
 export const environment = {
   production: true,
   staging: false,
-  isUnderMaintenance: false,
+  isUnderMaintenance: true,
   type: 'prod',
   baseUrl: 'http://techinterview.space',
   resourceApiURI: 'https://api.techinterview.space',
