10e603516006d92e20e3a860132e573778bf6bcd
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-02-16 16:11:24 +0600

    Made data clickable

--- src/app/modules/salaries/components/people-by-grades-chart/people-by-grades-chart.component.ts
+++ src/app/modules/salaries/components/people-by-grades-chart/people-by-grades-chart.component.ts
@@ -8,6 +8,8 @@ interface ProgressBarData {
   color: string;
   textColor: string;
   value: string;
+  width: number;
+  maxValue: string;
   label: string;
 }
 
@@ -34,35 +36,60 @@ export class PeopleByGradesChartComponent implements OnInit {
   barsForLocal: Array<ProgressBarData> = [];
   barsForRemote: Array<ProgressBarData> = [];
 
+  showPercents = true;
+
   @Input()
   title: string | null = null;
 
   constructor() {}
 
   ngOnInit(): void {
+    this.initCharts();
+  }
+
+  toggleCountAndPercent(): void {
+    this.showPercents = !this.showPercents;
+    this.initCharts();
+  }
+
+  private initCharts(): void {
     if (this.source == null) {
       return;
     }
 
     if (this.source.peopleByGradesChartDataForLocal != null) {
-      this.barsForLocal = this.prepareData(this.source.peopleByGradesChartDataForLocal);
+      this.barsForLocal = this.prepareData(
+        this.source.peopleByGradesChartDataForLocal, this.showPercents);
     }
 
     if (this.source.peopleByGradesChartDataForRemote != null) {
-      this.barsForRemote = this.prepareData(this.source.peopleByGradesChartDataForRemote);
+      this.barsForRemote = this.prepareData(
+        this.source.peopleByGradesChartDataForRemote, this.showPercents);
     }
   }
 
-  private prepareData(data: PeopleByGradesChartData): Array<ProgressBarData> {
+  private prepareData(data: PeopleByGradesChartData, showPercents: boolean): Array<ProgressBarData> {
     const totalCount = data.allCount;
+
     const result = data.data
       .filter((item) => item.grade !== DeveloperGrade.Unknown)
       .map((item, index) => {
         const color = this.colorsByGrade.get(item.grade) ?? PeopleByGradesChartComponent.defaultColor;
+        const width = (item.count / totalCount) * 100;
+        const value = showPercents
+          ? formatNumber(width, 'en-US', '1.0-2') + '%'
+          : formatNumber(item.count, 'en-US', '1.0-0');
+
+        const maxValue = showPercents
+          ? '100'
+          : formatNumber(totalCount, 'en-US', '1.0-0');
+
         return {
           color: color.color,
           textColor: color.textColor,
-          value: formatNumber((item.count / totalCount) * 100, 'en-US', '1.0-2'),
+          value: value,
+          maxValue: maxValue,
+          width: width,
           label: DeveloperGrade[item.grade],
         };
     });
@@ -70,11 +97,22 @@ export class PeopleByGradesChartComponent implements OnInit {
     const noGradeData = data.data.find((item) => item.grade === DeveloperGrade.Unknown);
     if (noGradeData != null) {
       const color = this.colorsByGrade.get(DeveloperGrade.Unknown) ?? PeopleByGradesChartComponent.defaultColor;
+      const width = (noGradeData.count / totalCount) * 100;
+      const value = showPercents
+          ? formatNumber(width, 'en-US', '1.0-2') + '%'
+          : formatNumber(noGradeData.count, 'en-US', '1.0-0');
+
+        const maxValue = showPercents
+          ? '100'
+          : formatNumber(totalCount, 'en-US', '1.0-0');
+
       result.push(
         {
           color: color.color,
           textColor: color.textColor,
-          value: formatNumber((noGradeData.count / totalCount) * 100, 'en-US', '1.0-2'),
+          value: value,
+          maxValue: maxValue,
+          width: width,
           label: "Грейд не указан",
         }
       );
