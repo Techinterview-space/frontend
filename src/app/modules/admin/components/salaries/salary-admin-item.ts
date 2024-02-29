c17dc4dbb623a669f12ea96db99603435fe3308b
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-02-29 21:26:14 +0600

    Admin table adjust

--- src/app/modules/admin/components/salaries/salary-admin-item.ts
+++ src/app/modules/admin/components/salaries/salary-admin-item.ts
@@ -1,4 +1,5 @@
 import { DeveloperGrade } from "@models/enums/developer-grade.enum";
+import { GenderEnum } from "@models/enums/gender.enum";
 import { CompanyType } from "@models/salaries/company-type";
 import { Currency } from "@models/salaries/currency";
 import { KazakhstanCity } from "@models/salaries/kazakhstan-city";
@@ -15,6 +16,9 @@ export class SalaryAdminItem {
     readonly company: string;
     readonly grade: string | null;
     readonly profession: string;
+    readonly gender: string;
+    readonly age: string;
+    readonly yearOfStartingWork: string;
     readonly city: string;
     readonly skill: string;
     readonly industry: string;
@@ -32,6 +36,9 @@ export class SalaryAdminItem {
         this.year = item.year;
         this.currency = Currency[item.currency];
         this.company = CompanyType[item.company];
+        this.age = item.age?.toString() ?? '-';
+        this.yearOfStartingWork = item.yearOfStartingWork?.toString() ?? '-';
+        this.gender = item.gender != null ? GenderEnum.label(item.gender) : '-';
         this.grade = item.grade != null ? DeveloperGrade[item.grade] : null;
         this.profession = professions.find(p => p.id == item.professionId)?.title || '-';
         this.city = item.city != null ? KazakhstanCity[item.city] : '-';
