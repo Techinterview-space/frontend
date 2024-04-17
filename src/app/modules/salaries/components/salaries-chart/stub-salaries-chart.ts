1937944f7b47814d699d7b49dea911c36e427463
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-04-17 12:23:00 +0500

    Fixed error

--- src/app/modules/salaries/components/salaries-chart/stub-salaries-chart.ts
+++ src/app/modules/salaries/components/salaries-chart/stub-salaries-chart.ts
@@ -17,8 +17,8 @@ export class StubSalariesChart extends SalariesChart {
         super({
             averageSalary: data?.averageSalary ?? StubSalariesChart.getRandomNumber(700, 300) * 1000,
             medianSalary: data?.medianSalary ?? StubSalariesChart.getRandomNumber(700, 300) * 1000,
-            averageRemoteSalary: StubSalariesChart.getRandomNumber(1_200, 600) * 1000,
-            medianRemoteSalary: StubSalariesChart.getRandomNumber(1_200, 600) * 1000,
+            averageRemoteSalary: data?.averageRemoteSalary ?? StubSalariesChart.getRandomNumber(1_200, 600) * 1000,
+            medianRemoteSalary: data?.medianRemoteSalary ?? StubSalariesChart.getRandomNumber(1_200, 600) * 1000,
             localSalariesByGrade: [],
             remoteSalariesByGrade: [],
             shouldAddOwnSalary: true,
