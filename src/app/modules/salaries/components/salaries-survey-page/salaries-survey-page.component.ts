090c77a60ac07556ba870c99e9acf71fff086c64
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-05-09 12:22:53 +0500

    Update salaries-survey-page.component.ts

--- src/app/modules/salaries/components/salaries-survey-page/salaries-survey-page.component.ts
+++ src/app/modules/salaries/components/salaries-survey-page/salaries-survey-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { GetUserSalariesSurveyDataResponse, SurveyService } from "@services/salaries-survey.service";
 import { TitleService } from "@services/title.service";
+import { AuthService } from "@shared/services/auth/auth.service";
+import { CookieService } from "ngx-cookie-service";
 import { untilDestroyed } from "@shared/subscriptions/until-destroyed";
 
 @Component({
@@ -14,17 +17,27 @@ export class SalariesSurveyPageComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly service: SurveyService,
+    private readonly router: Router,
+    private readonly authService: AuthService,
+    private readonly cookieService: CookieService,
     private readonly titleService: TitleService
   ) {
     titleService.setTitle("Опрос о пользе зарплатной статистики");
   }
  
   ngOnInit(): void {
-    this.service.getUserSalariesSurveyDataResponse()
-      .pipe(untilDestroyed(this))
-      .subscribe((data) => {
-        this.userData = data;
-      });
+     if (this.authService.isAuthenticated()) {
+       this.service.getUserSalariesSurveyDataResponse()
+        .pipe(untilDestroyed(this))
+        .subscribe((data) => {
+          this.userData = data;
+        });
+
+       return;
+    }
+
+    this.cookieService.set("url", this.router.url);
+    this.authService.login();
   }
 
   closeSurveyBlock(): void {}
