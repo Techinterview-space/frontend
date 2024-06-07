3e320d989f3dc1920f55fd250e8b23b070976c9e
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-06-07 11:52:32 +0500

    Gifs are replaced

--- src/app/modules/interviews/components/interviews-home/interviews-home.component.ts
+++ src/app/modules/interviews/components/interviews-home/interviews-home.component.ts
@@ -57,7 +57,7 @@ export class InterviewsHomeComponent implements OnInit, OnDestroy {
         subtitle:
           "Мы сохраним ваши заметки. Никто не сможет их посмотреть, кроме вас.",
         imageSrc:
-          "https://object.pscloud.io/petrel-intranet-storage/public-files/5738785c-4522-4842-861e-c2b59903b228-conduct_interview_main_page.gif",
+          "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/conduct_interview_main_page.gif",
         imageAlt: "Conduct the interview GIF",
         showPreloader: true,
       },
@@ -67,7 +67,7 @@ export class InterviewsHomeComponent implements OnInit, OnDestroy {
         subtitle:
           "Вы можете поделиться фидбеком как markdown-документом. А также экспортнуть и в PDF.",
         imageSrc:
-          "https://object.pscloud.io/petrel-intranet-storage/public-files/dfd35b14-26e8-4eab-bb3f-6ab4e69febfa-share_the_interview.gif",
+          "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/share_the_interview.gif",
         imageAlt: "Share the interview GIF",
         showPreloader: true,
       },
@@ -77,7 +77,7 @@ export class InterviewsHomeComponent implements OnInit, OnDestroy {
         subtitle:
           "Создайте шаблон для интервью с часто задаваемыми вопросами. А если вы хотите поделиться шаблоном, то можете сделать его публичным.",
         imageSrc:
-          "https://object.pscloud.io/petrel-intranet-storage/public-files/05dfc8cc-565f-4500-b4df-490175252e74-create_templates_main_page.gif",
+          "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/create_templates_main_page.gif",
         imageAlt: "Create interview templates GIF",
         showPreloader: true,
       },
@@ -87,7 +87,7 @@ export class InterviewsHomeComponent implements OnInit, OnDestroy {
         subtitle:
           "Вы сможете быстро перенести вопросы на интервью с ваших личных шаблонов и публичных тоже.",
         imageSrc:
-          "https://object.pscloud.io/petrel-intranet-storage/public-files/2aff4767-c3d1-4493-b138-de93b4383f16-use_templates_for_your_interview.gif",
+          "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/use_templates_for_your_interview.gif",
         imageAlt: "Use templates for your interviews GIF",
         showPreloader: true,
       },
