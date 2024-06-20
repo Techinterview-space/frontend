db37e9aa8d16755f3b8a40986bd9e953663db686
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-06-20 07:53:24 +0400

    adjusted

--- src/app/modules/salaries/components/historical-charts-page/historical-survey-chart/historical-survey-chart-object.ts
+++ src/app/modules/salaries/components/historical-charts-page/historical-survey-chart/historical-survey-chart-object.ts
@@ -1,31 +1,42 @@
 import { Chart, ChartType, PointStyle } from "chart.js/auto";
 import { RandomRgbColor } from "../../random-rgb-color";
-import { HistoricalSurveyChartResponse } from "@services/historical-charts.models";
+import { ExpectationPercentage, HistoricalSurveyChartResponse, SurveyResultsByWeeksChartItem } from "@services/historical-charts.models";
+import { ExpectationReplyType } from "@services/salaries-survey.service";
+import { RgbColor } from "../../rgb-color";
 
-interface ChartDatasetType {
-  label: string;
-  data: Array<number>;
-  borderWidth: number;
-  borderColor: string;
-  backgroundColor: string;
-  pointStyle: PointStyle;
-  yAxisID: "y" | "y1";
-}
 
 export class HistoricalSurveyChartObject extends Chart {
-  private readonly datasets: Array<ChartDatasetType> = [];
+  private readonly datasets: Array<DatasetItem> = [];
 
-  constructor(canvasId: string, chartData: HistoricalSurveyChartResponse) {
-    
+  constructor(
+    canvasId: string,
+    chartData: HistoricalSurveyChartResponse) {
+
     const data = chartData.surveyResultsByWeeksChart;
-    const datasets: Array<ChartDatasetType> = [
+    const datasets: Array<DatasetItem> = [
+      ...HistoricalSurveyChartObject.prepareExpectationDatasets(
+        data.items,
+        "Казахстанские компании",
+        "1",
+        0,
+        (x) => x.localExpectationPercentage
+      ),
+      ...HistoricalSurveyChartObject.prepareExpectationDatasets(
+        data.items,
+        "Удаленка",
+        "2",
+        50,
+        (x) => x.remoteExpectationPercentage
+      ),
       new DatasetItem(
         "Количество ответов",
         data.items.map((x) => x.totalCount),
         4,
         new RandomRgbColor(),
         "circle",
-        "y1"
+        "y1",
+        "line",
+        "0"
       ),
     ];
 
@@ -39,13 +50,18 @@ export class HistoricalSurveyChartObject extends Chart {
         responsive: true,
         maintainAspectRatio: false,
         scales: {
+          x: {
+            stacked: true,
+          },
           y: {
             beginAtZero: true,
             position: "left",
+            stacked: true,
           },
           y1: {
             beginAtZero: true,
             position: "right",
+            stacked: true,
           },
         },
         elements: {
@@ -66,19 +82,81 @@ export class HistoricalSurveyChartObject extends Chart {
 
     this.datasets = datasets;
   }
+
+  static prepareExpectationDatasets(
+    items: SurveyResultsByWeeksChartItem[],
+    postfix: string,
+    stack: string,
+    darken = 0,
+    dispatcher: (x: SurveyResultsByWeeksChartItem) => ExpectationPercentage[]
+  ): Array<DatasetItem> {
+    return [
+      new DatasetItem(
+        "Выше ожиданий, " + postfix,
+        items.map((x) => HistoricalSurveyChartObject
+          .getExpectationReplyType(ExpectationReplyType.MoreThanExpected, dispatcher(x))),
+        1,
+        RgbColor.green(darken),
+        false as PointStyle,
+        "y",
+        "bar",
+        stack,
+        1,
+        0.8
+      ),
+      new DatasetItem(
+        "Ожидаемо, " + postfix,
+        items.map((x) => HistoricalSurveyChartObject
+          .getExpectationReplyType(ExpectationReplyType.Expected, x.localExpectationPercentage)),
+        1,
+        RgbColor.blue(darken),
+        false as PointStyle,
+        "y",
+        "bar",
+        stack,
+        1,
+        0.8
+      ),
+      new DatasetItem(
+        "Ниже ожиданий, " + postfix,
+        items.map((x) => HistoricalSurveyChartObject
+          .getExpectationReplyType(ExpectationReplyType.LessThanExpected, x.localExpectationPercentage)),
+        1,
+        RgbColor.red(darken),
+        false as PointStyle,
+        "y",
+        "bar",
+        stack,
+        1,
+        0.8
+      ),
+    ];
+  }
+
+  static getExpectationReplyType(
+    type: ExpectationReplyType,
+    records: ExpectationPercentage[]
+  ) : number {
+    const record = records.find((x) => x.replyType === type);
+    return record ? record.percentage : 0;
+  }
 }
 
-class DatasetItem implements ChartDatasetType {
+class DatasetItem {
   constructor(
     readonly label: string,
     readonly data: number[],
     readonly borderWidth: number,
     readonly color: RandomRgbColor,
     readonly pointStyle: PointStyle,
-    readonly yAxisID: "y1" | "y"
+    readonly yAxisID: "y1" | "y",
+    readonly type: "line" | "bar",
+    readonly stack: string,
+    borderColorOpacity: number = 1,
+    bgColorOpacity: number = 0.5,
   ) {
-    this.borderColor = color.toString(1);
-    this.backgroundColor = color.toString(0.5);
+    this.borderColor = color.toString(borderColorOpacity);
+    this.backgroundColor = color.toString(bgColorOpacity);
   }
 
   readonly borderColor: string;
