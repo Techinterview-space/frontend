dd3f9b8304b36ee128da0e7df83be1eb12c77684
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-12-06 09:58:54 +0300

    Added cat

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-loading-spinner",
@@ -6,9 +6,22 @@ import { Component, Input } from "@angular/core";
   styleUrls: ["./loading-spinner.component.scss"],
   standalone: false,
 })
-export class LoadingSpinnerComponent {
+export class LoadingSpinnerComponent implements OnInit {
   @Input()
   style = "spinner-border-lg";
 
+  static LoadingCatUrl = "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/loading_cat.gif";
+  static LoadingDogUrl = "https://techinterview.fra1.cdn.digitaloceanspaces.com/gif/loading_dog.gif";
+
+  loadingUrl = '';
   showPreloader = false;
+
+  ngOnInit(): void {
+    const now = new Date();
+    const isOdd = now.getHours() % 2 === 1;
+
+    this.loadingUrl = isOdd
+      ? LoadingSpinnerComponent.LoadingCatUrl
+      : LoadingSpinnerComponent.LoadingDogUrl;
+  }
 }
